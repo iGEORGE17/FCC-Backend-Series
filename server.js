55a24c82593db7c83c4ac4a8fca614737ccd88f3
iGEORGE <www.igeorge.www@gmail.com>
2022-08-11 02:28:53 +0100

    create json object in json route

--- server.js
+++ server.js
@@ -27,6 +27,12 @@
  app.get('/', (req, res) => {
    res.sendFile(__dirname + '/views/index.html')
  })
+
+ app.get('/json', (req, res) => {
+    res.json({
+        "message": "Hello json"
+    })
+  })
  
  var port = process.env.PORT || 3000;
  bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
