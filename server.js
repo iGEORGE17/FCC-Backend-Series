7f097e1f1ef07ac9ab948036b75c169240a99aae
iGEORGE <www.igeorge.www@gmail.com>
2022-08-11 13:26:29 +0100

    added logger middleware

--- server.js
+++ server.js
@@ -22,9 +22,17 @@ require('dotenv').config();
    });
  }
 
+ app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip)
+  next();
+ })
+
+
  app.use(express.static(__dirname + "/public"))
  app.use("/public", express.static(__dirname + "/public"))
  
+
+
  app.get('/', (req, res) => {
    res.sendFile(__dirname + '/views/index.html')
  })
