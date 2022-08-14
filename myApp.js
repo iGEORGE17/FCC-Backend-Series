a139f2e1176bc318557f7d29aed520bbe2ff2ddf
iGEORGE <www.igeorge.www@gmail.com>
2022-08-14 12:12:48 +0100

    made changes

--- myApp.js
+++ myApp.js
@@ -4,15 +4,13 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
-
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true },
   age: Number,
   favoriteFoods: [String]
 });
 
-Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   let person = new Person({
